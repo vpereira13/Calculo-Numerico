2f12b7b7a1c10a4fe6e49f2c4db4f8c12a71c0c9
vpereira13 <victor.smp13@gmail.com>
2018-04-17 08:09:29 -0300

    Adicionado cabeçalho nos códigos fonte

--- Trabalho 1/Relatório/source_codes/gauss_seidel.h
+++ Trabalho 1/Relatório/source_codes/gauss_seidel.h
@@ -1,3 +1,14 @@
+/**
+ *	Trabalho 1 -  Métodos Iterativos para  Sitemas Lineares
+ *
+ *		Cálculo Numérico	SME-0104
+ *		Prof.: Murilo Francisco Tomé
+ *
+ *		Eduardo Brunaldi dos Santos				8642515
+ *		Jorge Ashkar Ferreira Simondi			8517081
+ *		Victor Luiz da Silva Mariano Pereira	8602444
+ */
+
 #ifndef GAUSS_SEIDEL_H
 #define GAUSS_SEIDEL_H
 
