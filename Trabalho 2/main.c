a79190b1d623491a4a78ae401a6b5e8890c480a7
Victor Luiz <victor.smp13@gmail.com>
2018-06-11 17:25:56 -0300

    Atualização

--- Trabalho 2/main.c
+++ Trabalho 2/main.c
@@ -13,15 +13,26 @@
 #include <simpson_e_newton.h>
 
 int main (int argc, char *argv[]){
+	int op;
 	long double x0;
 	long double xN;
 	long double n;
+	long double e;
 
-	scanf("%Lf", &x0);
-	scanf("%Lf", &xN);
-	scanf("%Lf", &n);
+	scanf("%d", &op);
 
-	printf("Valor da integral, utilizando Simpson 1/3 Composta: %.16Lf\n", simpson_composta(x0, xN, n, f_linha));
-	printf("Valor da raiz pelo metodo de Newton: %.16Lf\n", newton(0.5, 0.0000000001, simpson_composta, f_linha));
+	if (op == 0){
+		scanf("%Lf", &x0);
+		scanf("%Lf", &xN);
+		scanf("%Lf", &n);
+
+		printf("Valor da integral, utilizando Simpson 1/3 Composta: %.16Lf\n", simpson_composta(x0, xN, n, f_linha));
+	} else if (op == 1){
+		scanf("%Lf", &x0);
+		scanf("%Lf", &e);
+
+		printf("Valor da raiz pelo metodo de Newton: %.16Lf\n", newton(x0, e, simpson_composta, f_linha));
+	}
+
 	return 0;
 }
